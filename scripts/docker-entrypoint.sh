1c383c6e880a712653dceca5f3ae6f298ffece40
AndiMajore <andi.majore@googlemail.com>
2022-07-06 13:00:23 +0200

    precomputed network files
    Former-commit-id: 47918164504f861ee42f0a3a67b4885c66e99870 [formerly b61514f4b82e0781e65acfc0153fdadd0db77b74]
    Former-commit-id: 84aed50d8f61ef526add4515d69eb321abc595d7

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-file="store/docker-entrypoint.lock"
-# exit if entrypoint.lock exists to prevent new import of data every time docker is restarted
-
-
-
-#if ! test -f "$file"; then
-#    sh scripts/import-data.sh
-    python3 manage.py makemigrations drugstone
-    python3 manage.py migrate
-    python3 manage.py createfixtures
-    python3 manage.py cleanuptasks
-    python3 manage.py populate_db --all --update
-    python3 manage.py make_graphs
-#    touch $file
-#fi
+python3 manage.py makemigrations drugstone
+python3 manage.py migrate
+python3 manage.py createfixtures
+python3 manage.py cleanuptasks
+python3 manage.py populate_db --all --update
+python3 manage.py make_graphs
 
 /usr/bin/supervisord -c "/etc/supervisor/conf.d/supervisord.conf"
