5997ebaa2be2be0a24e4e743ba384bc429455d06
AndiMajore <andi.majore@googlemail.com>
2022-11-16 16:48:19 +0100

    tested and set celery_beat nedrex_update task
    Former-commit-id: b9676136ce0b04c16ab33e6af54bd9ad2dfbb872 [formerly f967adf4295d0b5579e3cab31635c19281fe9016]
    Former-commit-id: 063ec427c135b21ca4a05ab2324b21c9ea74f3e3

--- drugstone/settings/celery_schedule.py
+++ drugstone/settings/celery_schedule.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 CELERY_BEAT_SCHEDULE = {
     'update_db': {
         'task': 'drugstone.tasks.task_update_db_from_nedrex',
-        'schedule': crontab(day_of_week=1, hour=6, minute=0),
+        'schedule': crontab(day_of_week=1, hour=5, minute=0),
         # 'schedule': crontab(minute='*/1'),
     },
 }
