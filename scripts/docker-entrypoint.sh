e947fc1bf54a62188f73a2607fc136865b4f1e16
AndiMajore <andi.majore@googlemail.com>
2022-12-05 11:12:26 +0100

    fixed startup entry point
    Former-commit-id: 9d78ab5a10c0d8509008fed08360b697e5a51279 [formerly 52c9e12a3bf46c3fcca1b2ac7992398413f98712]
    Former-commit-id: 866e745a99c293c03896731eae0d6c89a4ceede6

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,9 +4,9 @@ python3 manage.py makemigrations drugstone
 python3 manage.py migrate
 python3 manage.py createfixtures
 python3 manage.py cleanuptasks
-if [[ -z "$DB_UPDATE_ON_START" ]] || [[ "$DB_UPDATE_ON_START" = "0" ]]
+if [ -z "$DB_UPDATE_ON_START" ] || [ "$DB_UPDATE_ON_START" = "0" ]
 then
-echo "Update on startup disabled!"
+ echo "Update on startup disabled!"
 else
  python3 manage.py populate_db --update -a
  python3 manage.py make_graphs
