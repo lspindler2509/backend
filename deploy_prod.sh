a58b8b370547d03323c148c1a33ba97a4bdc9c6e
Hartung, Michael <michael.hartung@uni-hamburg.de>
2023-04-22 00:22:49 +0200

    remove deploy scripts
    Former-commit-id: 39b3c48cc48898c3880f98ff74c886d04af215c4 [formerly e5cbbd4c4d006f3eda208fe9f14d4defedefde44]
    Former-commit-id: 6ad5478d907ab0f43e17ed4317068bce4c767335

--- deploy_prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "$branch" == "production" ]; then
-	docker build -t gitlab.rrz.uni-hamburg.de:4567/cosy-bio/drugst.one/backend:prod -f ./Dockerfile .
-	docker push gitlab.rrz.uni-hamburg.de:4567/cosy-bio/drugst.one/backend:prod
-else 
-	echo "DENIED: Your are not in the production branch. Do not push to production from the ${branch} branch."
-fi
