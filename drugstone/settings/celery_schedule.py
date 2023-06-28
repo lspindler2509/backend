ec4d4105bb21793fae257257ed405c338e8ffd3e
Hartung, Michael <michael.hartung@uni-hamburg.de>
2023-06-28 15:18:55 +0200

    disable weekly update task

--- drugstone/settings/celery_schedule.py
+++ drugstone/settings/celery_schedule.py
@@ -1,9 +1,9 @@
 from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
-    'update_db': {
-        'task': 'drugstone.tasks.task_update_db_from_nedrex',
-        'schedule': crontab(day_of_week=1, hour=22, minute=0),
-        # 'schedule': crontab(minute='*/1'),
-    },
+    # 'update_db': {
+    #     'task': 'drugstone.tasks.task_update_db_from_nedrex',
+    #     'schedule': crontab(day_of_week=1, hour=22, minute=0),
+    #     # 'schedule': crontab(minute='*/1'),
+    # },
 }
