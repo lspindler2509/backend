072d533b8983cbb1721d68b6690761801bb8e2de
AndiMajore <andi.majore@googlemail.com>
2024-01-11 14:39:29 +0100

    added stable configuration files and condition for update scheduling

--- drugstone/settings/celery_schedule.py
+++ drugstone/settings/celery_schedule.py
@@ -1,9 +1,19 @@
+import os
 from celery.schedules import crontab
 
-CELERY_BEAT_SCHEDULE = {
-    # 'update_db': {
-    #     'task': 'drugstone.tasks.task_update_db_from_nedrex',
-    #     'schedule': crontab(day_of_week=1, hour=22, minute=0),
-    #     # 'schedule': crontab(minute='*/1'),
-    # },
-}
+if os.environ.get("STABLE") == "1":
+    CELERY_BEAT_SCHEDULE = {
+        'update_db': {
+            'task': 'drugstone.tasks.task_update_db_from_nedrex',
+            'schedule': crontab(day_of_month=1, month_of_year=1, hour=22, minute=0),
+            # 'schedule': crontab(minute='*/1'),
+        },
+    }
+else:
+    CELERY_BEAT_SCHEDULE = {
+        'update_db': {
+            'task': 'drugstone.tasks.task_update_db_from_nedrex',
+            'schedule': crontab(day_of_week=1, hour=22, minute=0),
+            # 'schedule': crontab(minute='*/1'),
+        },
+    }
