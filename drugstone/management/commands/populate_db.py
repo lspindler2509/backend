944daddb2dda40fd523e19311cf01911db2d0dd7
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-06-30 16:40:11 +0200

    fixed requirements.txt

--- drugstone/management/commands/populate_db.py
+++ drugstone/management/commands/populate_db.py
@@ -122,7 +122,7 @@ def populate(kwargs):
     importer = NedrexImporter(nedrex_api_url_licensed, nedrex_api_url_open, cache)
     populator = DataPopulator(cache)
     importer.set_skip_due_to_duplicate_version(DatasetLoader.nedrex_version_duplicated())
-    print(f"Checked for NeDRex version already being present in database and result is: {DatasetLoader.nedrex_version_duplicated()}")
+    print(f"Checked for NeDRex version {DatasetLoader.get_nedrex_version()} already being present in database and result is: {DatasetLoader.nedrex_version_duplicated()}")
 
     total_n = 0
     nedrex_update = False
