8634526f04f33a6608f121ee84419d1b3ae1607f
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-07-11 17:55:45 +0200

    fixing another issue regarding reading edge attributes propperly for download networks

--- drugstone/management/commands/make_graphs.py
+++ drugstone/management/commands/make_graphs.py
@@ -475,7 +475,7 @@ def get_or_create_pdis_network(dataset, identifier, licensed, isReviewed, fmt):
             e = g.add_edge(disorder_vertices[id1], vertices[id2])
             n += 1
             e_type[e] = 'protein-disorder'
-            e_score[e] = e.score
+            e_score[e] = edge_raw.score
     print("done with protein-disorder edges: ", n)
 
     # remove unconnected proteins
@@ -612,9 +612,9 @@ def get_or_create_ppi_network(dataset, identifier, licensed, isReviewed, fmt):
             e = g.add_edge(vertices[id1], vertices[id2])
             n += 1
             e_type[e] = 'protein-protein'
-            e_directed[e] = e.is_directed
-            e_inhibition[e] = e.is_inhibition
-            e_stimulation[e] = e.is_stimulation
+            e_directed[e] = edge_raw.is_directed
+            e_inhibition[e] = edge_raw.is_inhibition
+            e_stimulation[e] = edge_raw.is_stimulation
     print("done with PPI edges: ", n)
 
     # remove unconnected proteins
