b2512957a12900aa08e51c686dfab2267dc42024
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-07-14 15:39:18 +0200

    trying to debug internal_id issues

--- drugstone/management/commands/make_graphs.py
+++ drugstone/management/commands/make_graphs.py
@@ -221,30 +221,30 @@ def get_or_create_pdi_network(dataset, identifier, licensed, isReviewed, fmt):
             g.vertex_properties["ensembl"] = v_ensembl
         print(f"Protein nodes:{len(node_id_map.items())}")
         done = 0
-        for id, internal_ids in node_id_map.items():
-            print(f"protein: {id}")
-            v = g.add_vertex()
-            v_type[v] = 'protein'
-            v_internal_id[v] = ",".join({f"p{id}" for id in internal_ids})
-            if is_internal:
-                node = drugstone_id_to_node[id]
+    for id, internal_ids in node_id_map.items():
+        print(f"protein: {id}")
+        v = g.add_vertex()
+        v_type[v] = 'protein'
+        v_internal_id[v] = ",".join({f"p{id}" for id in internal_ids})
+        if is_internal:
+            node = drugstone_id_to_node[id]
+            v_reviewed[v] = node.isReviewed
+            v_name[v] = node.gene
+            v_uniprot[v] = node.uniprot_code
+            v_symbol[v] = node.gene
+            v_entrez[v] = node.entrez
+            if node.id in ensembl_set.keys():
+                v_ensembl[v] = ",".join({f"{id}" for id in ensembl_set[node.id]})
+            done+=1
+            print(f"done: {done}/{len(node_id_map.items())}")
+            vertices[id] = v
+        else:
+            for drugstone_id in internal_ids:
+                print(f"\tfor {drugstone_id}")
+                node = drugstone_id_to_node[drugstone_id]
                 v_reviewed[v] = node.isReviewed
                 v_name[v] = node.gene
-                v_uniprot[v] = node.uniprot_code
-                v_symbol[v] = node.gene
-                v_entrez[v] = node.entrez
-                if node.id in ensembl_set.keys():
-                    v_ensembl[v] = ",".join({f"{id}" for id in ensembl_set[node.id]})
-                done+=1
-                print(f"done: {done}/{len(node_id_map.items())}")
-                vertices[id] = v
-            else:
-                for drugstone_id in internal_ids:
-                    print(f"\tfor {drugstone_id}")
-                    node = drugstone_id_to_node[drugstone_id]
-                    v_reviewed[v] = node.isReviewed
-                    v_name[v] = node.gene
-                    vertices[drugstone_id] = v
+                vertices[drugstone_id] = v
 
 
     for node in models.Drug.objects.all():
