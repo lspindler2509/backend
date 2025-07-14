f3b0a8fd0df25dc6ca9b47374ca7ff8a83b376ff
Andreas Maier <andreas.maier-1@uni-hamburg.de>
2025-07-14 15:27:04 +0200

    trying to debug internal_id issues

--- drugstone/management/commands/make_graphs.py
+++ drugstone/management/commands/make_graphs.py
@@ -225,21 +225,26 @@ def get_or_create_pdi_network(dataset, identifier, licensed, isReviewed, fmt):
             v = g.add_vertex()
             v_type[v] = 'protein'
             v_internal_id[v] = ",".join({f"p{id}" for id in internal_ids})
-            for drugstone_id in internal_ids:
-                print(f"\tfor {drugstone_id}")
-                node = drugstone_id_to_node[drugstone_id]
-                v_reviewed[v] = node.isReviewed
-                v_name[v] = node.gene
-                vertices[drugstone_id] = v
             if is_internal:
                 print("\tfor internal")
                 node = drugstone_id_to_node[id]
+                v_reviewed[v] = node.isReviewed
+                v_name[v] = node.gene
                 v_uniprot[v] = node.uniprot_code
                 v_symbol[v] = node.gene
                 v_entrez[v] = node.entrez
                 if node.id in ensembl_set.keys():
                     v_ensembl[v] = ",".join({f"{id}" for id in ensembl_set[node.id]})
                 print(f"done")
+                vertices[id] = v
+            else:
+                for drugstone_id in internal_ids:
+                    print(f"\tfor {drugstone_id}")
+                    node = drugstone_id_to_node[drugstone_id]
+                    v_reviewed[v] = node.isReviewed
+                    v_name[v] = node.gene
+                    vertices[drugstone_id] = v
+
 
     for node in models.Drug.objects.all():
         v = g.add_vertex()
